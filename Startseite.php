24239d97ccc585795ddd2e0c3936e96c66632c85
Frozz3 <105207692+Frozz3@users.noreply.github.com>
2022-12-16 15:07:27 +0100

    fsdfs
    sdfsd

--- Startseite.php
+++ Startseite.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:Startseite.php
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -61,67 +60,4 @@
     </div>
     </footer>
 </body>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Startseite.css">
-    <title>Document</title>
-</head>
-<body>
-    <header>
-        <div class="img"><img src="images/geige.png" alt="geige"></div>
-        <div id="header">
-            <h1>Private Musikschule T&uuml;bingen</h1>
-        </div>
-        <nav class="menu">
-            <a href="Startseite.html">Start</a> <a href="Instrumente.html">Instrumente</a> <a href="Unterricht.html">Unterricht</a> <a href="Kontakt.html">Kontakt</a>
-            
-        </nav>
-    </header>
-    <main>
-        <div id="Begrüßung">
-           <p>Die Tübinger Musikschule bietet Kindern, Jugendlichen und Erwachsenen Unterricht
-            in allen klassischen Instrumenten an: Holz- und Blechbläser, Streich-, Tasten- und 
-            Zupfinstrumente, Schlaginstrumente und Percussion. Ebenso werden die Vokalfächer
-            Sologesang und Jazz- und Pop-Gesang unterrichtet.
-            Das Kursprogramm richtet sich an die Jüngsten und bietet Rhythmik ab einem Jahr, 
-            elementare Musikerziehung, ein Instrumenten-Karussell, Blockflötenkurse, Blechbläser- 
-            und Holzbläservorschulen, Trommeln für Kids und einen Kinderchor.
-        </p>
-    </div>
-        <img id="KlavierKreis" class="blockLook MainLook" style="box-shadow: none;" src="images/Startseite Bild.jpg" alt="">
-        <div id="Information" class="MainLook">
-            <lo>
-                <h4>Öffnungszeiten:</h4>
-                <li>Montag - Freitag 9.30 bis 12 Uhr</li>
-                <li>Montag - Donnerstag 14.30 bis 16.30 Uhr</li>
-            </lo>
-        
-            <lo>
-                <h4>Anschrift:</h4>
-                <li>Universitätsstadt Tübingen</li>
-                <li>Tübinger Musikschule</li>
-                <li>Frischlinstraße 4</li>
-                <li>72074 Tübingen</li>
-            </lo>
-           
-        </div>
-    </main>
-    
-    
-    <footer class="blockLook">
-        
-        <div id="text">
-        <p>Private Musikschule T&uuml;bingen</p> 
-        <p>Telefon: 07071 2046100</p>
-        <p>Adresse: Frischlinstraße 4, 72074 Tübingen</p>
-        <small><a href="impressum.html">Impressum</a> <a href="Datenschutz.html">Datenschutz</a></small>
-    </div>
-    </footer>
-</body>
->>>>>>> 31c8c2743dc30928a815a255b0bfa9c7e7b40aae:Startseite.html
 </html>
