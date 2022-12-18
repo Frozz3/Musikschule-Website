f9a6632d2cf160ade6996c8c8695dbfe77bee2d5
Frozz3 <105207692+Frozz3@users.noreply.github.com>
2022-12-18 19:05:37 +0100

    d

--- Admin Login.php
+++ Admin Login.php
@@ -1,4 +1,28 @@
 
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+
+    <header>
+        <div class="img"><img src="images/geige.png" alt="geige"></div>
+        <div id="header">
+            <h1>Private Musikschule T&uuml;bingen</h1>
+        </div>
+        <nav class="menu">
+            <a href="Startseite.php">Start</a> <a href="login.php">Instrumente</a> <a href="Unterricht.php">Unterricht</a> <a href="Kontakt.php">Kontakt</a>
+            
+        </nav>
+    </header>
+
     <?php
     session_start();
 
@@ -55,3 +79,12 @@
 
 
 ?>
+<footer class="blockLook">
+        
+        <div id="text">
+        <p>Private Musikschule T&uuml;bingen</p> 
+        <p>Telefon: 07071 2046100</p>
+        <p>Adresse: Frischlinstraße 4, 72074 Tübingen</p>
+        <small><a href="impressum.html">Impressum</a> <a href="Datenschutz.html">Datenschutz</a></small>
+    </div>
+    </footer>
--- Adminbereich.php
+++ Adminbereich.php
@@ -1,3 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+
+    <header>
+        <div class="img"><img src="images/geige.png" alt="geige"></div>
+        <div id="header">
+            <h1>Private Musikschule T&uuml;bingen</h1>
+        </div>
+        <nav class="menu">
+            <a href="Startseite.php">Start</a> <a href="login.php">Instrumente</a> <a href="Unterricht.php">Unterricht</a> <a href="Kontakt.php">Kontakt</a>
+            
+        </nav>
+    </header>
+
 <?php
 	
 	echo '<h1>Seiteninhalt bearbeiten</font></h1>';
@@ -22,4 +46,14 @@
 	echo '<br /><br />';
 	echo '<input type="submit" value="Inhalt speichern" />';
 	echo '</form>';
+?>
+	<footer class="blockLook">
+        
+        <div id="text">
+        <p>Private Musikschule T&uuml;bingen</p> 
+        <p>Telefon: 07071 2046100</p>
+        <p>Adresse: Frischlinstraße 4, 72074 Tübingen</p>
+        <small><a href="impressum.html">Impressum</a> <a href="Datenschutz.html">Datenschutz</a></small>
+    </div>
+    </footer>
 
